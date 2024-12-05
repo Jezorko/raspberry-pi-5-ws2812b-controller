f3311e5a94144e8e6afe6768b42be73ae24a470d
Paweł Szymański <pawelfiu@gmail.com>
2024-12-05 22:06:39 +0100

    Cleanup & hope for the best

--- src/main.rs
+++ src/main.rs
@@ -1,156 +1,117 @@
-mod timings;
-mod test_extensions;
-mod strip;
-mod instructions;
-mod fuckme;
-
-use crate::strip::{create_strip, LedController};
-use crate::timings::{get_signal_representation_in_bytes, DEFAULT_WS2812B_TIMING_REQUIREMENTS};
-use bitvec::order::{BitOrder, Lsb0, Msb0};
-use rppal::spi::BitOrder::{LsbFirst, MsbFirst};
+use embedded_hal::spi::SpiBus;
 use rppal::spi::{Bus, Mode, SlaveSelect, Spi};
-
-use embedded_hal::spi::{ErrorKind, ErrorType, SpiBus};
-use rppal::gpio::Gpio;
-use smart_leds::{SmartLedsWrite, RGB8};
 use std::error::Error;
-use std::fmt::{Debug, Display, Formatter, Write};
-use std::{thread, time};
+use std::thread;
 use std::time::Duration;
-use ws2812_spi::Ws2812;
-use crate::fuckme::fuck_me;
 
-/// 8MHz == 125ns
-const SPI_CLOCK_SPEED: u32 = 8_000_000;
-
-struct Color {
-    red: u8,
-    green: u8,
-    blue: u8,
+pub trait LedStripController {
+    /// Size of the LED Strip.
+    fn len(&self) -> usize;
+    /// Turn all LEDs off.
+    fn reset(&mut self);
+    /// Set the LED at given position to the desired RGB value.
+    fn set(&mut self, position: usize, red: u8, green: u8, blue: u8);
+    /// Set all LEDs in the strip to the desired RGB value.
+    fn set_all(&mut self, red: u8, green: u8, blue: u8);
+    /// Writes data to the LED strip.
+    fn commit(&mut self) -> Result<(), Box<dyn Error>>;
 }
 
-impl Display for Color {
-    fn fmt(&self, formatter: &mut Formatter<'_>) -> std::fmt::Result {
-        formatter.write_fmt(format_args!("R:{} G:{} B:{}", self.red, self.green, self.blue))
+struct SpiLedStripController {
+    leds_count: usize,
+    /// Raw data to be sent to SPI.
+    buffer: Vec<u8>,
+    /// SPI device to which the strip is connected.
+    spi: Spi,
+}
+
+impl LedStripController for SpiLedStripController {
+    fn len(&self) -> usize {
+        self.buffer.len() / (8 * 3/* 8 bits per color, 3 colors */)
+    }
+
+    fn reset(&mut self) {
+        self.set_all(0, 0, 0);
+    }
+
+    fn set(&mut self, position: usize, red: u8, green: u8, blue: u8) {
+        fn bitmask(value: u8, position: usize) -> bool {
+            value & (1 << (7 - position)) != 0
+        }
+
+        let mut current_byte_position = position * (8 * 3/* 8 bits per color, 3 colors */);
+
+        // TODO: extract this loop
+        // set green
+        for i in 0..8 {
+            if (bitmask(green, i)) {
+                self.buffer[current_byte_position] = 0xF8;
+            } else {
+                self.buffer[current_byte_position] = 0xC0
+            };
+            current_byte_position = current_byte_position + 1;
+        }
+        // set red
+        for i in 0..8 {
+            if (bitmask(red, i)) {
+                self.buffer[current_byte_position] = 0xF8;
+            } else {
+                self.buffer[current_byte_position] = 0xC0
+            };
+            current_byte_position = current_byte_position + 1;
+        }
+        // set blue
+        for i in 0..8 {
+            if (bitmask(blue, i)) {
+                self.buffer[current_byte_position] = 0xF8;
+            } else {
+                self.buffer[current_byte_position] = 0xC0
+            };
+            current_byte_position = current_byte_position + 1;
+        }
+    }
+
+    fn set_all(&mut self, red: u8, green: u8, blue: u8) {
+        for led_index in 0..self.len() {
+            self.set(led_index, red, green, blue);
+        }
+    }
+
+    fn commit(&mut self) -> Result<(), Box<dyn Error>> {
+        self.spi.write(&self.buffer[0..self.buffer.len()])?;
+        self.spi.flush()?;
+        Ok(())
     }
 }
 
-fn test_pin() -> Result<(), Box<dyn Error>> {
-    // testing GPIO 10
-    let mut pin = Gpio::new()?.get(10)?.into_output();
+fn create_ws2812b_strip(leds_count: usize) -> Result<impl LedStripController, Box<dyn Error>> {
+    let mut buffer = vec![0; (8 * 3/* 8 bits per color, 3 colors per LED */) * leds_count];
+    let spi_speed_khz: u32 = 800;
+    let spi_speed_bps: u32 = spi_speed_khz * 1024 * 8; // Convert kHz to bytes per second (TODO: why???)
+    let mut spi = Spi::new(Bus::Spi0, SlaveSelect::Ss0, spi_speed_bps, Mode::Mode0)?;
+    thread::sleep(Duration::from_millis(100)); // Short delay to ensure device is ready (TODO: needed???)
 
-    println!("Testing if correct GPIO pin is connected");
-    pin.toggle();
-    thread::sleep(Duration::from_millis(1_000));
-    pin.toggle();
-    thread::sleep(Duration::from_millis(1_000));
-    pin.toggle();
-    thread::sleep(Duration::from_millis(1_000));
-    pin.toggle();
-    thread::sleep(Duration::from_millis(1_000));
-    pin.toggle();
-    thread::sleep(Duration::from_millis(1_000));
-    pin.toggle();
-    println!("Testing done!");
-
-    Ok(())
+    Ok(SpiLedStripController {
+        leds_count,
+        buffer,
+        spi,
+    })
 }
-
-struct SpiAdapter(Spi);
-
-impl ErrorType for SpiAdapter { type Error = ErrorKind; }
-
-impl SpiBus<u8> for SpiAdapter {
-    fn read(&mut self, words: &mut [u8]) -> Result<(), Self::Error> {
-        self.0.read(words).map_err(|error| ErrorKind::Other)?;
-        Ok(())
-    }
-
-    fn write(&mut self, words: &[u8]) -> Result<(), Self::Error> {
-        self.0.write(words).map_err(|error| ErrorKind::Other)?;
-        Ok(())
-    }
-
-    fn transfer(&mut self, read: &mut [u8], write: &[u8]) -> Result<(), Self::Error> {
-        self.0.read(read).map_err(|error| ErrorKind::Other)?;
-        self.0.write(write).map_err(|error| ErrorKind::Other)?;
-        Ok(())
-    }
-
-    fn transfer_in_place(&mut self, words: &mut [u8]) -> Result<(), Self::Error> {
-        self.0.write(words).map_err(|error| ErrorKind::Other)?;
-        self.0.read(words).map_err(|error| ErrorKind::Other)?;
-        Ok(())
-    }
-
-    fn flush(&mut self) -> Result<(), Self::Error> {
-        self.0.flush().map_err(|error| ErrorKind::Other)?;
-        Ok(())
-    }
-}
-
-const LEDS_COUNT: usize = 5;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    fuck_me()
-}
+    let mut strip = create_ws2812b_strip(12)?;
 
-fn test_all() -> Result<(), Box<dyn Error>> {
-    test_pin()?;
-
-    // Configure the SPI peripheral. The 24AA1024 clocks in data on the first
-    // rising edge of the clock signal (SPI mode 0). At 3.3 V, clock speeds of up
-    // to 10 MHz are supported.
-    let mut spi = Spi::new(Bus::Spi0, SlaveSelect::Ss0, 1, Mode::Mode0)?;
-
-    let bit_order = spi.bit_order()?;
-    println!("bit order is {}", bit_order);
-
-    println!("testing with spi MSB; strip MSB");
-    spi.set_bit_order(MsbFirst)?;
-    test_strip::<Msb0>(&mut spi)?;
-
-    println!("testing with spi MSB; strip LSB");
-    spi.set_bit_order(MsbFirst)?;
-    test_strip::<Lsb0>(&mut spi)?;
-
-    println!("testing with spi LSB; strip MSB");
-    spi.set_bit_order(LsbFirst)?;
-    test_strip::<Msb0>(&mut spi)?;
-
-    println!("testing with spi LSB; strip LSB");
-    spi.set_bit_order(LsbFirst)?;
-    test_strip::<Lsb0>(&mut spi)?;
-
-    Ok(())
-}
-
-pub fn test_strip<DataBitsOrdering>(spi: &mut Spi) -> Result<(), Box<dyn Error>>
-where
-    DataBitsOrdering: BitOrder,
-{
-    let mut strip = create_strip::<DataBitsOrdering>(3, get_signal_representation_in_bytes(SPI_CLOCK_SPEED, DEFAULT_WS2812B_TIMING_REQUIREMENTS));
-
-    strip.write_to_spi_blocking(spi)?;
+    strip.set_all(0, 255, 0);
+    strip.commit()?;
     thread::sleep(Duration::from_secs(1));
 
+    strip.set_all(255, 0, 0);
+    strip.commit()?;
+    thread::sleep(Duration::from_secs(1));
 
-    [
-        Color { red: 255, green: 0, blue: 0 },
-        Color { red: 0, green: 255, blue: 0 },
-        Color { red: 0, green: 0, blue: 255 },
-        // Color { red: 255, green: 255, blue: 0 },
-        // Color { red: 255, green: 0, blue: 255 },
-        // Color { red: 0, green: 255, blue: 255 },
-        // Color { red: 150, green: 0, blue: 150 },
-        // Color { red: 100, green: 240, blue: 150 }
-    ].iter().for_each(|color| {
-        println!("setting color to {}", color);
-        strip.set_all_leds(color.red, color.green, color.blue);
-        strip.print_buffer();
-        strip.write_to_spi_blocking(spi).unwrap();
-        thread::sleep(Duration::from_secs(5));
-    });
+    strip.set_all(0, 255, 255);
+    strip.commit()?;
+    thread::sleep(Duration::from_secs(1));
 
     Ok(())
 }
